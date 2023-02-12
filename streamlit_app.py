fe53c135cf66a08eb66b50a794aff377e363ec9e
Subham Mohanty <48358012+subham58@users.noreply.github.com>
2023-02-13 01:15:45 +0530

    Update streamlit_app.py
    Latest 3 lines added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('My Parents new healthy dinner')
+streamlit.title('Omega 3 and Blueberry Oatmeal')
+streamlit.title('Kale, spinach and rocket smoothie')
+streamlit.title('Hard-Boiled Free-Range Egg')
