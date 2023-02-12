fab26866032fe67a43859701c3123c66bc1ee088
Subham Mohanty <48358012+subham58@users.noreply.github.com>
2023-02-13 01:18:57 +0530

    Update streamlit_app.py
    breakfast keyword added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthy dinner')
+streamlit.title('Breakfast Menu')
 streamlit.title('Omega 3 and Blueberry Oatmeal')
 streamlit.title('Kale, spinach and rocket smoothie')
 streamlit.title('Hard-Boiled Free-Range Egg')
